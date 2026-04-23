542a3af7dd91130d3d6eab86aebaf6422d871b6e
Lord_prei <93540498+Lord-prei@users.noreply.github.com>
2026-04-23 06:31:59 +0200

    Pause Menu
    Added Pause Menu (W.I.P.)

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -1,14 +1,50 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour
 {
-    public static bool isPaused = false;
+    // --------------------------------------------------------------------------- Variables
 
+    #region Public Variables
+
+    // Singleton instance for easy access
     public static PauseScript Instance;
-
     void Awake()
     {
         Instance = this;
+    }
+
+    public static bool isPaused = false;
+
+    public UIDocument PausMenuUI;
+
+    #endregion
+
+    // --------------------------------------------------------------------------- Private Variables
+
+    #region Private Variables
+
+    // Private variables go here
+
+    #endregion
+
+    // --------------------------------------------------------------------------- Functions
+
+    #region Functions
+
+    // -------------------------------------------------- Pause / Resume Logic
+
+    #region Pause / Resume Logic
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
     }
 
     public void TogglePause()
@@ -21,13 +57,67 @@ public class PauseScript : MonoBehaviour
             Resume();
     }
 
-    void Pause()
+    #endregion
+
+    // -------------------------------------------------- UI Logic
+
+    #region UI Logic
+    void VisablePauseMenu(string what)
     {
-        Time.timeScale = 0f;
+        if (what == "show")
+        {
+            PausMenuUI.rootVisualElement.style.display = DisplayStyle.Flex;
+        }
+        else if (what == "hide")
+        {
+            PausMenuUI.rootVisualElement.style.display = DisplayStyle.None;
+        }
     }
 
-    void Resume()
+    // Code for Functions Layer 2
+
+    #endregion
+
+    // -------------------------------------------------- XXX
+
+    #region Cat3
+
+    // Code for Functions Layer 3
+
+    #endregion
+
+    #endregion Functions
+
+    // --------------------------------------------------------------------------- Start and Update
+
+    #region Unity Lifecycle Methods
+
+    void Start()
     {
-        Time.timeScale = 1f;
+        VisablePauseMenu("hide");
     }
+
+    void Update()
+    {
+        if (isPaused)
+        {
+            VisablePauseMenu("show");
+        }
+        else
+        {
+            VisablePauseMenu("hide");
+        }
+    }
+
+    #endregion
+
+    // --------------------------------------------------------------------------- Collision and Trigger Events
+
+    #region Collision and Trigger Events
+
+
+
+    #endregion
+
+    // --------------------------------------------------------------------------- End of Script
 }
