735e79f4c32cf390d7c90a2c5d4eafc2dfba3330
Lord_prei <93540498+Lord-prei@users.noreply.github.com>
2026-06-04 12:40:39 +0200

    Task work
    Add Task to container TaskLists

--- Assets/Editor/To-Do/ToDo.cs
+++ Assets/Editor/To-Do/ToDo.cs
@@ -8,7 +8,7 @@ using System;
 public class ToDo : EditorWindow
 {
     [MenuItem("Window/UI Toolkit/ToDo")]
-    public static void ShowExample()
+    public static void Init()
     {
         ToDo wnd = GetWindow<ToDo>();
         wnd.titleContent = new GUIContent("To-Do");
@@ -20,26 +20,32 @@ public class ToDo : EditorWindow
         public string title;
         public string description;
         public bool desFolded;
+        public string path = "root";
 
         public override string ToString()
         {
-            return $"TodoItemData\ndone =\t{done}\ntitle =\t\t{title}\ndescr =\t{description}\ndesFolded =\t{desFolded}";
+            return $"TodoItemData\ndone =\t{done}\ntitle =\t\t{title}\ndescr =\t{description}\ndesFolded =\t{desFolded}\npath =\t{path}";
         }
     }
 
-    private VisualElement CreateHorizontalFoldout(ToDoItemData data)
+    private VisualElement CreateHorizontalFoldout(ToDoItemData data, TextField parentTitle)
     {
         // Create a new VisualElement to represent the horizontal foldout
         VisualElement root = new VisualElement();
+        root.name = "foldoutRoot";
         root.style.flexDirection = FlexDirection.Row;
+        root.style.flexGrow = 0;
 
         // Content of the foldout
         VisualElement content = new VisualElement();
+        content.name = "foldoutContent";
         content.style.flexDirection = FlexDirection.Row;
         content.style.flexGrow = 1;
+        content.style.flexBasis = Length.Percent(100);
 
         // Description field for the task
         TextField description = new TextField();
+        description.name = "Description";
         description.value = data.description;
         description.style.flexDirection = FlexDirection.Row;
         description.style.flexGrow = 1;
@@ -52,14 +58,17 @@ public class ToDo : EditorWindow
 
         // Label for Description
         Label descriptionLabel = new Label("Desc:");
+        descriptionLabel.name = "descriptionLabel";
         descriptionLabel.style.unityTextAlign = TextAnchor.MiddleLeft;
 
         // Foldout button
         Button foldoutButton = new Button();
+        foldoutButton.name = "foldoutButton";
         foldoutButton.clickable.clicked += () =>
         {
-            ToggleFoldout();
+            ToggleFoldout(false);
         };
+        ToggleFoldout(true);
 
         // Structure of object:
         // -------------------- Root
@@ -72,19 +81,24 @@ public class ToDo : EditorWindow
 
         return root; // Ensure a VisualElement is returned to fix CS0161
 
-        void ToggleFoldout()
+        void ToggleFoldout(bool init)
         {
-            data.desFolded = !data.desFolded;
+            if (!init)
+                data.desFolded = !data.desFolded;
             //change Symbol on button
             if (data.desFolded) //Folded
             {
+                root.style.flexGrow = 0; // Don't take up space when folded
                 foldoutButton.text = "▶"; // Folded symbol
                 content.style.display = DisplayStyle.None; // Hide content
+                parentTitle.style.display = DisplayStyle.Flex; // Show title when folded
             }
             else // Unfolded
             {
+                root.style.flexGrow = 1; // Take up space when unfolded
                 foldoutButton.text = "▼"; // Unfolded symbol
                 content.style.display = DisplayStyle.Flex; // Show content
+                parentTitle.style.display = DisplayStyle.None; // Hide title when unfolded
             }
             //Debug.Log(data);
         }
@@ -93,11 +107,14 @@ public class ToDo : EditorWindow
     {
         // Create a new VisualElement to represent the to-do item
         VisualElement row = new VisualElement();
+        row.name = "TaskRoot";
         row.style.flexDirection = FlexDirection.Row;
+        row.style.width = Length.Percent(100);
 
         // Create an Event Listener to save input in data when the user interacts with the UI
         // Toggle for marking the task as done
         Toggle done = new Toggle();
+        done.name = "CheckBox";
         done.value = data.done;
         done.RegisterValueChangedCallback(evt =>
         {
@@ -108,12 +125,13 @@ public class ToDo : EditorWindow
 
         // Title field for the task
         TextField title = new TextField();
+        title.name = "TaskName";
         title.value = data.title;
         title.style.flexGrow = 1;
         title.RegisterValueChangedCallback(evt =>
         {
             data.title = evt.newValue;
-            //Debug.Log(data);
+            Debug.Log(data);
         });
 
 
@@ -122,13 +140,13 @@ public class ToDo : EditorWindow
         {
             rootVisualElement.Remove(row);
         });
+        deleteButton.name = "deleteButton";
         deleteButton.text = "X";
+        deleteButton.style.marginLeft = Length.Auto();
 
-        VisualElement foldOut = CreateHorizontalFoldout(data);
-        {
-            //Debug.Log("Foldout button clicked!");
-            //Debug.Log(data);
-        };
+
+        // Create the horizontal foldout for the description
+        VisualElement foldOut = CreateHorizontalFoldout(data, title);
 
 
         // Add the UI elements to the row
@@ -141,27 +159,61 @@ public class ToDo : EditorWindow
 
         return row; // Ensure a VisualElement is returned to fix CS0161
     }
+    private VisualElement CreateFolder()
+    {
+        VisualElement root = new VisualElement();
+        root.name = "FolderRoot";
+
+
+        return root;
+    }
     
     public void CreateGUI()
     {
         VisualElement root = rootVisualElement;
-        ShowExample();
+        root.name = "Root";
 
-        Button addButton = new Button(() =>
+        VisualElement ControlMenu = new VisualElement();
+        ControlMenu.name = "ControlMenu";
+        ControlMenu.style.flexDirection = FlexDirection.Row;
+        ControlMenu.style.width = Length.Percent(100);
+
+        VisualElement TaskList = new VisualElement();
+        TaskList.name = "TaskList";
+        TaskList.style.flexDirection = FlexDirection.Row;
+        TaskList.style.width = Length.Percent(100);
+
+        Init();
+
+        Button button_AddTask = new Button(() =>
         {
             ToDoItemData initVal = new ToDoItemData();
             initVal.done = false;
             initVal.title = $"New Task {rootVisualElement.childCount}";
             initVal.description = $"New Description {rootVisualElement.childCount}";
-            initVal.desFolded = false;
+            initVal.desFolded = true;
 
             root.Add(CreateToDoItemElement(initVal));
         });
+        button_AddTask.name = "AddTaskButton";
+        button_AddTask.text = "Add Task";
+        button_AddTask.style.width = Length.Percent(50);
 
 
-        addButton.text = "Add Task";
-        root.Add(addButton);
+        Button button_AddFolder = new Button(() =>
+        {
+            Debug.Log("Add Folder");
+        });
+        button_AddFolder.name = "AddFolderButton";
+        button_AddFolder.text = "Add Folder";
+        button_AddFolder.style.width = Length.Percent(50);
 
-        
+
+        root.Add(ControlMenu);
+        ControlMenu.Add(button_AddTask);
+        ControlMenu.Add(button_AddFolder);
+
+        root.Add(TaskList);
+
     }
 }
